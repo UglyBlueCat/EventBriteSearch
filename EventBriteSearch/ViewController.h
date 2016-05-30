bed76949bef29a8f8e01420782fe03d9ea09f158
Robin Spinks <uglybluecat@googlemail.com>
2016-05-30 14:53:02 +0100

    Initial Commit

---
+++ EventBriteSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EventBriteSearch
+//
+//  Created by Robin Spinks on 30/05/2016.
+//  Copyright © 2016 Robin Spinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EventBriteSearch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EventBriteSearch
+//
+//  Created by Robin Spinks on 30/05/2016.
+//  Copyright © 2016 Robin Spinks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
